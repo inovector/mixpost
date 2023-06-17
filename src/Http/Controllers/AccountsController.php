8640defa9fed71b6d04bb413b7cb09aa15597fb6
Dumitru Botezatu <dima.botezatu@gmail.com>
2023-06-17 13:18:21 +0300

    fix accounts list of is_configured_service

--- src/Http/Controllers/AccountsController.php
+++ src/Http/Controllers/AccountsController.php
@@ -4,6 +4,7 @@ namespace Inovector\Mixpost\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Arr;
 use Inertia\Inertia;
 use Inertia\Response;
 use Inovector\Mixpost\Actions\UpdateOrCreateAccount;
@@ -20,7 +21,7 @@ class AccountsController extends Controller
     {
         return Inertia::render('Accounts/Accounts', [
             'accounts' => AccountResource::collection(Account::latest()->get())->resolve(),
-            'is_configured_service' => Services::isConfigured(),
+            'is_configured_service' => Arr::except(Services::isConfigured(), ['unsplash', 'tenor']),
         ]);
     }
 
