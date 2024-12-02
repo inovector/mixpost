193f92574134e237539b221fa6959cbe6b6c2803
Dumitru Botezatu <dima.botezatu@gmail.com>
2024-12-02 17:59:38 +0200

    wip

--- resources/js/Components/Pro/UpgradePro.vue
+++ resources/js/Components/Pro/UpgradePro.vue
@@ -3,12 +3,12 @@ import {ref} from "vue";
 import DialogModal from "../Modal/DialogModal.vue";
 import LockOpen from "../../Icons/LockOpen.vue";
 import Flex from "../Layout/Flex.vue";
-import PrimaryButton from "../Button/PrimaryButton.vue";
 import MenuDelimiter from "../Sidebar/MenuDelimiter.vue";
 import Check from "../../Icons/Check.vue";
 import QuestionMarkCircle from "../../Icons/QuestionMarkCircle.vue";
 import PureButton from "../Button/PureButton.vue";
 import ProLabel from "./ProLabel.vue";
+import UnlockButton from "./UnlockButton.vue";
 
 const show = ref(false);
 
@@ -109,16 +109,7 @@ const features = [
             <p class="mt-xs">Mixpost Pro gives powerful tools for efficient social media management, saving time and boosting
                 collaboration for impactful content creation.</p>
 
-            <a href="https://mixpost.app/pricing?utm_medium=software&utm_source=mixpost&utm_campaign=ad_upgrade"
-               target="_blank" class="block w-full mt-md">
-                <PrimaryButton
-                    class="w-full !text-center justify-center bg-gradient-to-r from-purple-500 to-indigo-500 hover:drop-shadow-xl transition-all ease-in-out duration-200">
-                    <template #icon>
-                        <LockOpen/>
-                    </template>
-                    Unlock with Mixpost Pro
-                </PrimaryButton>
-            </a>
+            <UnlockButton/>
 
             <MenuDelimiter class="mt-md"/>
 
