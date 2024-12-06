f9c7928fd554d5cf7f547f0586ac8f626b5b8dd5
Dumitru Botezatu <dima.botezatu@gmail.com>
2024-12-06 21:07:21 +0200

    built assets

--- resources/dist/vendor/mixpost/assets/Error-Dmcqt4E2.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as t,b as s,a as o,ch as c,t as n,g as r}from"./app-Bb-WeEdh.js";const l={key:0,class:"inline-flex items-start text-red-500 leading-loose text-sm"},m={class:"mr-xs"},d={__name:"Error",props:["message"],setup(e){return(i,_)=>e.message?(a(),t("p",l,[s("span",m,[o(c,{class:"!w-5 !-5"})]),s("span",null,n(e.message),1)])):r("",!0)}};export{d as _};
