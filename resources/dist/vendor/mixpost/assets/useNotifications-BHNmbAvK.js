f8321989222ab7687db7a0214211070d953eeb42
mhamidawad <mhamidawad@users.noreply.github.com>
2025-07-02 17:56:22 +0000

    Build assets

--- resources/dist/vendor/mixpost/assets/useNotifications-BHNmbAvK.js
+++
@@ -1,3 +0,0 @@
-import{c4 as n}from"./app-DEeAE-ho.js";const j=()=>({notify:(o,t,i)=>{if(typeof t!="object"&&n.emit("notify",{variant:o,message:t,button:i}),typeof t=="object"){const f=Object.keys(t).map(c=>t[c].join(`
-`)).join(`
-`);n.emit("notify",{variant:o,message:f,button:i})}}});export{j as u};
